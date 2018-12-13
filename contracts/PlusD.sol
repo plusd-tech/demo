4186917417ba727d469dba44ce01bdfbcbe27509
willclarktech <willclarktech@users.noreply.github.com>
2018-12-14 00:01:49 +0100

    :seedling: Add registerConsignor function to PlusD

--- contracts/PlusD.sol
+++ contracts/PlusD.sol
@@ -3,6 +3,9 @@ pragma solidity ^0.4.23;
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
 contract PlusD is Ownable {
-	constructor() public {
+	mapping (bytes32 => address) public consignors;
+
+	function registerConsignor(address _consignor, bytes32 _companyRegistrationNumber) public {
+		consignors[_companyRegistrationNumber] = _consignor;
 	}
 }

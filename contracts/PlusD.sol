678902aac0578eacc2cad2c9f65a6139933f9f77
willclarktech <willclarktech@users.noreply.github.com>
2018-12-14 00:08:19 +0100

    :seedling: Add registerCarrier function to PlusD

--- contracts/PlusD.sol
+++ contracts/PlusD.sol
@@ -4,8 +4,13 @@ import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
 contract PlusD is Ownable {
 	mapping (bytes32 => address) public consignors;
+	mapping (bytes32 => address) public carriers;
 
 	function registerConsignor(address _consignor, bytes32 _companyRegistrationNumber) public onlyOwner {
 		consignors[_companyRegistrationNumber] = _consignor;
+	}
+
+	function registerCarrier(address _carrier, bytes32 _companyRegistrationNumber) public onlyOwner {
+		carriers[_companyRegistrationNumber] = _carrier;
 	}
 }

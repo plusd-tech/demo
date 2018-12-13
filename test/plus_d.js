09221000b5aa46bca46114a5d958573963ed280d
willclarktech <willclarktech@users.noreply.github.com>
2018-12-14 00:01:26 +0100

    :white_check_mark: Add test for PlusD registering consignor

--- test/plus_d.js
+++ test/plus_d.js
@@ -14,4 +14,26 @@ describe("PlusD", () => {
 			})
 		});
 	});
+
+	contract('Register consignor', ([owner, consignor]) => {
+		describe('Given the owner has initialised the contract', () => {
+			let plusD;
+
+			before(async () => {
+				plusD = await PlusD.deployed();
+			});
+
+			describe('When the owner registers a consignor using its address and company registration number', () => {
+				const companyRegistrationNumber = '27814';
+
+				before(async () => {
+					await plusD.registerConsignor(consignor, companyRegistrationNumber);
+				});
+
+				it('Then the consignor should be registered under the company registration number', async () => {
+					assert.strictEqual(await plusD.consignors(companyRegistrationNumber), consignor);
+				});
+			});
+		});
+	});
 });

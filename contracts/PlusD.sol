243d263d4f785184e7e29c9150db2cf360ce198d
willclarktech <willclarktech@users.noreply.github.com>
2018-12-14 00:10:50 +0100

    :seedling: Add registerInsurer function to PlusD

--- contracts/PlusD.sol
+++ contracts/PlusD.sol
@@ -5,6 +5,7 @@ import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 contract PlusD is Ownable {
 	mapping (bytes32 => address) public consignors;
 	mapping (bytes32 => address) public carriers;
+	mapping (bytes32 => address) public insurers;
 
 	function registerConsignor(address _consignor, bytes32 _companyRegistrationNumber) public onlyOwner {
 		consignors[_companyRegistrationNumber] = _consignor;
@@ -12,5 +13,9 @@ contract PlusD is Ownable {
 
 	function registerCarrier(address _carrier, bytes32 _companyRegistrationNumber) public onlyOwner {
 		carriers[_companyRegistrationNumber] = _carrier;
+	}
+
+	function registerInsurer(address _insurer, bytes32 _companyRegistrationNumber) public onlyOwner {
+		insurers[_companyRegistrationNumber] = _insurer;
 	}
 }

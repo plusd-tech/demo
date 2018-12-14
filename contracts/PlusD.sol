005ba1c76b01a126266d859c09d11da3db08b737
willclarktech <willclarktech@users.noreply.github.com>
2018-12-14 15:59:28 +0100

    :seedling: Reorder mappings and add onlyConsignor modifier to createConsignment

--- contracts/PlusD.sol
+++ contracts/PlusD.sol
@@ -4,9 +4,9 @@ import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "./Consignment.sol";
 
 contract PlusD is Ownable {
-	mapping (bytes32 => address) public consignors;
-	mapping (bytes32 => address) public carriers;
-	mapping (bytes32 => address) public insurers;
+	mapping (address => bytes32) public consignors;
+	mapping (address => bytes32) public carriers;
+	mapping (address => bytes32) public insurers;
 	mapping (address => address[]) public consignments;
 
 	event ConsignorRegistered(address consignor, bytes32 companyRegistrationNumber);
@@ -14,22 +14,27 @@ contract PlusD is Ownable {
 	event InsurerRegistered(address insurer, bytes32 companyRegistrationNumber);
 	event ConsignmentCreated(address consignment);
 
+	modifier onlyConsignor() {
+		require(consignors[msg.sender] != 0);
+		_;
+	}
+
 	function registerConsignor(address _consignor, bytes32 _companyRegistrationNumber) public onlyOwner {
-		consignors[_companyRegistrationNumber] = _consignor;
+		consignors[_consignor] = _companyRegistrationNumber;
 		emit ConsignorRegistered(_consignor, _companyRegistrationNumber);
 	}
 
 	function registerCarrier(address _carrier, bytes32 _companyRegistrationNumber) public onlyOwner {
-		carriers[_companyRegistrationNumber] = _carrier;
+		carriers[_carrier] = _companyRegistrationNumber;
 		emit CarrierRegistered(_carrier, _companyRegistrationNumber);
 	}
 
 	function registerInsurer(address _insurer, bytes32 _companyRegistrationNumber) public onlyOwner {
-		insurers[_companyRegistrationNumber] = _insurer;
+		insurers[_insurer] = _companyRegistrationNumber;
 		emit InsurerRegistered(_insurer, _companyRegistrationNumber);
 	}
 
-	function createConsignment(address _carrier, bytes32 _requirements) public {
+	function createConsignment(address _carrier, bytes32 _requirements) public onlyConsignor {
 		Consignment consignment = new Consignment(msg.sender, _carrier, _requirements);
 		consignments[msg.sender].push(consignment);
 		emit ConsignmentCreated(consignment);

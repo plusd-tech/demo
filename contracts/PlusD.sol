70b07edebff4594b4190c0de001a1cdbd920913b
willclarktech <willclarktech@users.noreply.github.com>
2018-12-14 13:06:09 +0100

    :seedling: Add function for creating a consignmnet from PlusD

--- contracts/PlusD.sol
+++ contracts/PlusD.sol
@@ -1,11 +1,13 @@
 pragma solidity ^0.4.23;
 
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "./Consignment.sol";
 
 contract PlusD is Ownable {
 	mapping (bytes32 => address) public consignors;
 	mapping (bytes32 => address) public carriers;
 	mapping (bytes32 => address) public insurers;
+	mapping (address => address[]) public consignments;
 
 	event ConsignorRegistered(address consignor, bytes32 companyRegistrationNumber);
 	event CarrierRegistered(address carrier, bytes32 companyRegistrationNumber);
@@ -24,5 +26,10 @@ contract PlusD is Ownable {
 	function registerInsurer(address _insurer, bytes32 _companyRegistrationNumber) public onlyOwner {
 		insurers[_companyRegistrationNumber] = _insurer;
 		emit InsurerRegistered(_insurer, _companyRegistrationNumber);
+	}
+
+	function createConsignment(address _carrier, bytes32 _requirements) public {
+		Consignment consignment = new Consignment(_carrier, _requirements);
+		consignments[msg.sender].push(consignment);
 	}
 }

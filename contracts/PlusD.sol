cdfb81cdb2d8aaa81f9efa286a4e90e8ae162791
willclarktech <willclarktech@users.noreply.github.com>
2018-12-17 15:57:38 +0100

    :seedling: Check consignee is registered at consignment creation

--- contracts/PlusD.sol
+++ contracts/PlusD.sol
@@ -3,19 +3,22 @@ pragma solidity ^0.4.23;
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "./Consignment.sol";
 
-contract PlusD is Ownable {
+contract PlusDRegistration is Ownable {
 	mapping (address => bytes32) public consignors;
 	mapping (address => bytes32) public consignees;
 	mapping (address => bytes32) public verifiers;
-	mapping (address => address[]) public consignments;
 
 	event ConsignorRegistered(address consignor, bytes32 companyRegistrationNumber);
 	event ConsigneeRegistered(address consignee, bytes32 companyRegistrationNumber);
 	event VerifierRegistered(address verifier, bytes32 companyRegistrationNumber);
-	event ConsignmentCreated(address consignment);
 
 	modifier onlyConsignor() {
 		require(consignors[msg.sender] != 0);
+		_;
+	}
+
+	modifier isConsignee(address _consignee) {
+		require(consignees[_consignee] != 0);
 		_;
 	}
 
@@ -33,10 +36,18 @@ contract PlusD is Ownable {
 		verifiers[_verifier] = _companyRegistrationNumber;
 		emit VerifierRegistered(_verifier, _companyRegistrationNumber);
 	}
+}
 
-	function createConsignment(address _consignee, bytes32 _requirements) public onlyConsignor {
+contract PlusDConsignments is PlusDRegistration {
+	mapping (address => address[]) public consignments;
+	event ConsignmentCreated(address consignment);
+
+	function createConsignment(address _consignee, bytes32 _requirements) public onlyConsignor isConsignee(_consignee) {
 		Consignment consignment = new Consignment(msg.sender, _consignee, _requirements);
 		consignments[msg.sender].push(consignment);
 		emit ConsignmentCreated(consignment);
 	}
+}
+
+contract PlusD is PlusDConsignments {
 }

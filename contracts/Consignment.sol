0be7daa481bab095a5756ed9a6c757e8e73f95b7
willclarktech <willclarktech@users.noreply.github.com>
2018-12-18 12:59:52 +0100

    :seedling: Do not assign consignee on consignment creation

--- contracts/Consignment.sol
+++ contracts/Consignment.sol
@@ -3,11 +3,11 @@ pragma solidity ^0.4.23;
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
 contract Consignment is Ownable {
-	uint8 private UNINITIALISED = 0x00;
+	uint8 private CONSIGNMENT_CREATED = 0x00;
 	uint8 private CONSIGNEE_ASSIGNED = 0x01;
 	uint8 private VERIFIER_ASSIGNED = 0x02;
 	uint8 private REQUIREMENTS_VERIFIED = 0x03;
-	uint8 public state = UNINITIALISED;
+	uint8 public state = CONSIGNMENT_CREATED;
 
 	address public consignor;
 	address public consignee;
@@ -19,10 +19,9 @@ contract Consignment is Ownable {
 	event VerifierAssigned(address verifier);
 	event RequirementsVerified();
 
-	constructor(address _consignor, address _consignee, bytes32 _requirements) public {
+	constructor(address _consignor, bytes32 _requirements) public {
 		consignor = _consignor;
 		requirements = _requirements;
-		assignConsignee(_consignee);
 	}
 
 	function assignConsignee(address _consignee) public onlyOwner {
